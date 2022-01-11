520faa72c4dc28a973bf121d6f6165ad339aee03
yanntyb <yanntyb.lbc@gmail.com>
2022-01-11 14:11:12 +0100

    fini

---
+++ assets/main.ts
@@ -0,0 +1,33 @@
+import "./style/style.scss";
+
+import "./images/logo.jpg";
+
+/**
+ * container 1
+ */
+import "./images/icons/birthday.png";
+import "./images/banner.jpg";
+
+/**
+ * container 2
+ */
+import "./images/icons/calendar.png";
+import "./images/icons/balloon.png"
+import "./images/icons/karaoke.png"
+
+/**
+ * container 3
+ */
+import "./images/slider/slider-1.jpg";
+import "./images/slider/slider-2.jpg";
+import "./images/slider/slider-3.jpg";
+import "./images/slider/slider-4.jpg";
+import "./images/slider/slider-5.jpg";
+import "./images/slider/slider-6.jpg";
+import "./images/slider/slider-7.jpg";
+import "./images/slider/slider-8.jpg";
+
+/**
+ * container 6
+ */
+import "./images/icons/email.png";
